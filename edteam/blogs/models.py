e3c599d98ad9e9032ad8881ba975a480934c3fe1
CARDONA LUIS <kikecar97@gmail.com>
2024-12-17 14:05:12 -0600

    v1.0.1

--- edteam/blogs/models.py
+++ edteam/blogs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Articulo(models.Model):
+    titulo = models.CharField(max_length=200)
+    imagen = models.ImageField(upload_to='articulos', null=True, blank=True)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.titulo
--- edteam/edteam/settings.py
+++ edteam/edteam/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#para subir imagenes a la base de datos
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
