42cc7e4438bfc2fc72d467836086b8e6b70bb6b9
CARDONA LUIS <kikecar97@gmail.com>
2024-12-18 09:48:23 -0600

    update

--- edteam/blogs/admin.py
+++ edteam/blogs/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Articulo
 
 # Register your models here.
-admin.site.register(Articulo)
+
+
+@admin.register(Articulo)
+class ArticuloAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'imagen', 'autor', 'fecha_creacion', 'fecha_modificacion')
